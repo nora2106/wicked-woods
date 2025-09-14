816b3a767f6aec5522bb89c14691172591c5cbda
nora <noraklinger@outlook.de>
2025-09-15 00:40:28 +0200

    implement new clock puzzle algorithm

--- Assets/Scripts/Puzzles/Clock.cs
+++ Assets/Scripts/Puzzles/Clock.cs
@@ -2,160 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.Linq;
 
 public class Clock : MonoBehaviour
 {
-    public int id;
     // clock time in minutes
     public int time;
-    public List<Clock> neighbors = new List<Clock>();
-    ClockPuzzleSimulation simulation;
-    System.Random rnd = new System.Random();
-    TextMeshPro text;
-    public LineRenderer lineRenderer;
+    // clock color type (1-3)
+    public int typeID;
+    // defines if clock can be edited
+    public bool editable = false;
+    public bool isMain = false;
+    TextMeshProUGUI text;
 
     void Start()
     {
-        text = gameObject.GetComponent<TextMeshPro>();
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.black;
-        lineRenderer.endColor = Color.black;
-        lineRenderer.startWidth = 0.05f;
-        lineRenderer.endWidth = 0.05f;
+        text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
+        if(isMain)
+        {
+            text.color = Color.red;
+        }
     }
 
     private void Update()
     {
-        text.text = time.ToString();
+        text.text = formatTime(time);
     }
 
     private void OnMouseDown()
     {
-        text.faceColor = Color.red;
-        lineRenderer.startColor = Color.red;
-        lineRenderer.endColor = Color.red;
-        neighbors.ForEach(n =>
+        if(editable)
         {
-            n.text.faceColor = Color.red;
-        });
-    }
-
-    public Clock(int ID, ClockPuzzleSimulation parentSim, int initialTime = 0)
-    {
-        id = ID;
-        simulation = parentSim;
-        if (initialTime != 0)
-        {
-            time = initialTime;
+            // edit time
+            time = 75;
+            gameObject.GetComponentInParent<ClockPuzzleSimulation>().UpdateAllClocks(this);
         }
     }
 
-    public void Setup(ClockPuzzleSimulation parentSim, int initialTime = 0)
+    public string formatTime(int time)
     {
-        simulation = parentSim;
-        if (initialTime != 0)
-        {
-            time = initialTime;
-        }
-        lineRenderer.positionCount = 1;
-
-        lineRenderer.SetPosition(0, transform.position);
-    }
-
-    // add clock as neighbor
-    public void addNeighbor(Clock newNeighbor)
-    {
-        neighbors.Add(newNeighbor);
-        UpdateLine();
-    }
-
-    public void UpdateLine()
-    {
-        lineRenderer.positionCount = 2;
-        lineRenderer.SetPosition(0, neighbors[0].transform.position);
-        lineRenderer.SetPosition(1, transform.position);
-
-        //if (lineRenderer.positionCount > 3)
-        //{
-        //    lineRenderer.SetPosition(0, neighbors[1].transform.position);
-        //    lineRenderer.SetPosition(1, transform.position);
-        //    lineRenderer.SetPosition(2, neighbors[2].transform.position);
-        //}
-    }
-
-    // add new neighbor with a time that completes the sum
-    public void FillSumWithNewNeighbor()
-    {
-        if(neighbors.Count == 0)
-        {
-            Debug.Log(time + " has no neighbors");
-            return;
-        }
-
-        int neighborSum(int i)
-        {
-            int sum = i;
-            neighbors.ForEach(n =>
-            {
-                sum += n.time;
-            });
-            return sum;
-        }
-
-        int generateNewTime()
-        {
-            int newTime = simulation.generateRandomTime();
-
-            if (neighborSum(newTime) == time || neighborSum(newTime) % 720 == time % 720)
-            {
-                return newTime;
-            }
-            return generateNewTime();
-        }
-
-        int requiredTime = generateNewTime();
-
-        if (simulation.addedClocks.Any(c => c.time == requiredTime) && simulation.loops > 5)
-        {
-            Debug.Log(time + " requires " + requiredTime);
-            //simulation.AddClock(this, requiredTime);
-            //simulation.addedClocks.ForEach(clock => simulation.clocks.Add(clock));
-            //simulation.reachedGoal = true;
-            //return;
-        }
-
-        if (requiredTime == time)
-        {
-            // add 2 clocks instead of 1
-            void calcTime()
-            {
-                int time1 = simulation.generateRandomTime(15, (requiredTime + 1));
-                int time2 = requiredTime - time1;
-                if(time2 <= 0 || time1 <= 0)
-                {
-                    calcTime();
-                    return;
-                }
-                simulation.AddClock(this, time1);
-                simulation.AddClock(this, time2);
-            }
-            calcTime();
-            return;
-
-        }
-        simulation.AddClock(this, requiredTime);
-        return;
-    }
-
-    // check if the sum of all neighbors results in the current time
-    public bool checkIfTimeWorks()
-    {
-        int timeSum = 0;
-        neighbors.ForEach(n =>
-        {
-            timeSum += n.time;
-        });
-        return timeSum % 720 == time % 720;
+        int hour = time / 60;
+        int minutes = time - (hour * 60);
+        string timeString = hour.ToString("00") + ":" + minutes.ToString("00");
+        return timeString;
     }
 }
--- Assets/Scripts/Puzzles/ClockPuzzleSimulation.cs
+++ Assets/Scripts/Puzzles/ClockPuzzleSimulation.cs
@@ -1,106 +1,152 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Linq;
+
 public class ClockPuzzleSimulation : MonoBehaviour
 {
     public GameObject clockPrefab;
-    public List<Clock> clocks = new List<Clock>();
-    public List<Clock> addedClocks = new List<Clock>();
+    // all clocks per type, including main clocks
+    public List<Clock> clocks1 = new List<Clock>();
+    public List<Clock> clocks2 = new List<Clock>();
+    public List<Clock> clocks3 = new List<Clock>();
+    public List<List<Clock>> allClocks;
+    
+    // only main clocks
+    public List<Clock> mainClocks = new List<Clock>();
     System.Random rnd = new System.Random();
-    bool first = true;
-    public bool reachedGoal = false;
-    public int loops = 0;
+    public int startTime;
+    public TextMeshProUGUI realTime;
+    public int globalTime;
 
     void Start()
     {
-        //GenerateClocks();
-        Debug.Log(AddTimes(450, 450));
+        InitializeClocks();
+        globalTime = startTime;
+        int hour = globalTime / 60;
+        int minutes = globalTime - (hour * 60);
+        realTime.text = hour.ToString("00") + ":" + minutes.ToString("00");
+        CalcMainClockTimes();
+        CalcClockTimes();
     }
 
-    int AddTimes(int time1, int time2, int time3 = 0)
+    int AddClockTimes(List<Clock> clocks)
     {
-        return (time1 + time2 + time3) % 720;
+        int sum = 0;
+        clocks.ForEach(clock =>
+        {
+            sum += clock.time;
+        });
+        if(sum == 720)
+        {
+            return 720;
+        }
+        return sum % 720;
+    }
+
+    private void InitializeClocks()
+    {
+        allClocks = new List<List<Clock>> { clocks1, clocks2, clocks3 };
+        foreach (Transform t in gameObject.transform)
+        {
+            if (t.gameObject.GetComponent<Clock>())
+            {
+                Clock clock = t.gameObject.GetComponent<Clock>();
+                allClocks[clock.typeID - 1].Add(clock);
+                if (clock.isMain)
+                {
+                    mainClocks.Add(clock);
+                }
+            }
+        }
+    }
+
+    private void CalcMainClockTimes()
+    {
+        int attempts = 0;
+        int maxAttempts = 10000;
+
+        while (AddClockTimes(mainClocks) != globalTime && attempts < maxAttempts)
+        {
+            for (int i = 0; i < mainClocks.Count; i++)
+            {
+                mainClocks[i].time = GenerateRandomTime();
+            }
+            attempts++;
+        }
+
+        if (attempts >= maxAttempts)
+        {
+            Debug.LogWarning("Error: Infinite while loop.");
+        }
+    }
+
+    private void CalcClockTimes()
+    {
+        int attempts = 0;
+        int maxAttempts = 10000;
+
+        for (int i = 0; i < allClocks.Count; i++)
+        {
+            while (allClocks[i].Any(c => c.time == 0) && attempts < maxAttempts)
+            {
+                for (int j = 0; j < allClocks[i].Count; j++)
+                {
+                    if(allClocks[i][j].time == 0)
+                    {
+                        int newTime = 0;
+                        if (j < (allClocks[i].Count - 1) && allClocks[i][j + 1].time != 0)
+                        {
+                            newTime = (allClocks[i][j + 1].time + 720 + 60) % 720;
+                        }
+
+                        else if (j > 0 && allClocks[i][j - 1].time != 0)
+                        {
+                            newTime = (allClocks[i][j - 1].time + 720 + 60) % 720;
+                        }
+                        allClocks[i][j].time = newTime;
+                    }
+                }
+                attempts++;
+            }
+            if (attempts >= maxAttempts)
+            {
+                Debug.LogWarning("Error: Infinite while loop.");
+            }
+        }
+    }
+
+    public void UpdateAllClocks(Clock changedClock)
+    {
+        List<Clock> currClocks = allClocks[changedClock.typeID - 1];
+        for (int i = 0; i < currClocks.Count; i++)
+        {
+            if(i < (currClocks.Count - 1) && currClocks[i].time != (currClocks[i + 1].time + 720 + 60) % 720)
+            {
+                currClocks[i].time = (currClocks[i + 1].time + 720 + 60) % 720;
+            }
+            else if (i > 0 && currClocks[i].time != (currClocks[i - 1].time + 720 + 60) % 720)
+            {
+                currClocks[i].time = (currClocks[i - 1].time + 720 + 60) % 720;
+            }
+
+        }
+
+        globalTime = AddClockTimes(mainClocks);
+        int hour = globalTime / 60;
+        int minutes = globalTime - (hour * 60);
+        realTime.text = hour.ToString("00") + ":" + minutes.ToString("00");
     }
 
     // generate a random time in minutes between 15 (00:15) and 720 (24:00)
-    public int generateRandomTime(int x = 15, int y = 721)
+    public int GenerateRandomTime(int x = 15, int y = 721)
     {
         int randomNum = rnd.Next(x, y);
         if (randomNum % 15 == 0)
         {
             return randomNum;
         }
-        return generateRandomTime();
-    }
-
-    void GenerateClocks()
-    {
-        GameObject clockObject = Instantiate(clockPrefab, transform.position, Quaternion.identity);
-        Clock firstClock = clockObject.GetComponent<Clock>();
-        firstClock.Setup(this, 720);
-        clocks.Add(firstClock);
-
-        GameObject clockObjec2t = Instantiate(clockPrefab, new Vector3(transform.position.x - 3 , transform.position.y, 0), Quaternion.identity);
-        Clock clock2 = clockObjec2t.GetComponent<Clock>();
-        clock2.Setup(this, 450);
-        clocks.Add(clock2);
-        firstClock.addNeighbor(clock2);
-        clock2.addNeighbor(firstClock);
-        
-        firstClock.FillSumWithNewNeighbor();
-        clock2.FillSumWithNewNeighbor();
-
-        while (!reachedGoal && loops < 20)
-        {
-            loops += 1;
-            clocks.ForEach(clock =>
-            {
-                if (!clock.checkIfTimeWorks())
-                {
-                    clock.FillSumWithNewNeighbor();
-                }
-            });
-            addedClocks.ForEach(clock => clocks.Add(clock));
-            addedClocks = new List<Clock>();
-        }
-    }
-
-    public void AddClock(Clock baseClock, int time = 0)
-    {
-        Vector3 basePos = baseClock.gameObject.transform.position;
-        float newX = basePos.x + 0.5f;
-        float newY = basePos.y + 0.5f;
-        if (first)
-        {
-            newX = basePos.x - 0.5f;
-        }
-        first = !first;
-        
-        GameObject clockObject = Instantiate(clockPrefab, new Vector3(newX, newY), Quaternion.identity);
-        Clock newClock = clockObject.GetComponent<Clock>();
-        newClock.Setup(this, time);
-        newClock.addNeighbor(baseClock);
-        baseClock.addNeighbor(newClock);
-        addedClocks.Add(newClock);
-    }
-
-    private bool allClocksCorrect()
-    {
-        bool clocksChecked = true;
-        clocks.ForEach(clock =>
-        {
-            if (!clock.checkIfTimeWorks())
-            {
-                clocksChecked = false;
-            }
-        });
-        addedClocks.ForEach(clock =>
-        {
-            if (!clock.checkIfTimeWorks())
-            {
-                clocksChecked = false;
-            }
-        });
-        return clocksChecked;
+        return GenerateRandomTime();
     }
 }
