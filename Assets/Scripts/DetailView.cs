f5175acd044228e572cd2fbeb59f8cf93139c6d1
nora <noraklinger@outlook.de>
2025-09-15 20:05:59 +0200

    bobos house scene setup + bug fix

--- Assets/Scripts/DetailView.cs
+++ Assets/Scripts/DetailView.cs
@@ -9,7 +9,7 @@ public class DetailView : MonoBehaviour
     public Button closeBtn;
     public GameObject obj;
     private GameManager gm;
-    // Start is called before the first frame update
+    
     void Start()
     {
         transform.gameObject.SetActive(false);
